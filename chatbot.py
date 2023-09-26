a2d2f310e786838dc2d4be3f823ac58d55a37c55
AIzoo-Jamlab <145838186+AIzooLab@users.noreply.github.com>
2023-09-26 12:57:55 +0200

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+import openai
+
+openai.api_key = config['openai_api_key']
+
+def get_chatbot_response(user_input, context=""):
+    prompt = f"{context}\nUser: {user_input}\nChatBot:"
+    openai_response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=50
+    )
+    assistant_response = openai_response.choices[0].text.strip()
+    return assistant_response
